5f9c6cb77c02c4d8dce6acd343c0bcd11b80c517
Adrian Dahlin <akd361@nyu.edu>
2017-07-26 20:45:11 -0400

    script inputs workout details for a single day and adds these to a csv

--- fitness.py
+++ fitness.py
@@ -5,11 +5,49 @@ csv_file_path = "workouts.csv"
 rows = []
 
 with open(csv_file_path, "w") as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=["date", "run", "bike", "sports", "yoga", "abs"])
+    writer = csv.DictWriter(csv_file, fieldnames=["date", "run", "bike", "sports", "yoga", "abs", "lift"])
     writer.writeheader()
-    for row in rows:
-        writer.writerow(row)
+    writer.writerow({"date": "07/26/17", "run": "0", "bike": "0", "sports": ".5", "yoga": "0", "abs": "0", "lift": ".1"})
 
-menu = "Choose one of the following operations: list, show month, show today"
+with open(csv_file_path, "r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        rows.append(row)
+        print(row["date"], row["sports"])
+
+menu = "Please enter the following operation: new workout "
+operations = ["new workout"]
 
 def command_center():
+    inp = input(menu)
+    if inp == "new workout":
+        new_workout()
+    if inp not in operations:
+        print("I'm sorry, I don't recognize that operation. I'll try to figure it out soon!")
+
+def new_workout():
+    print("Enter your workout details here:")
+    date = input("Date (mm/dd/yy): ")
+    run = input("Run (miles): ")
+    bike = input("Bike (miles): ")
+    sports = input("Sports (hours): ")
+    yoga = input("Yoga (hours): ")
+    abdoms = input("Abs (minutes): ")
+    lift = input("Lift (0 to 1): ")
+    new_workout = {
+        "date": date,
+        "run": run,
+        "bike": bike,
+        "sports": sports,
+        "yoga": yoga,
+        "abs": abdoms,
+        "lift": lift
+    }
+    with open(csv_file_path, "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=["date", "run", "bike", "sports", "yoga", "abs", "lift"])
+        writer.writeheader()
+        for row in rows:
+            writer.writerow(row)
+        writer.writerow(new_workout)
+
+command_center()
